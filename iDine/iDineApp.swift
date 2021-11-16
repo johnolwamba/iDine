5cc87e37d9f227a456076a3c925c62be40009325
Johnstone Ananda <johnolwamba@gmail.com>
2021-11-16 00:02:29 -0600

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Johnstone Ananda on 11/16/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
